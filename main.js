16aa16e22aa8676a9f2f9027727f3abeafd07164
Bojan Bojanic <q.qat.01@gmail.com>
2018-08-21 10:46:34 +0200

    102

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello test from jenkins build\n");
+  response.end("Hello test from jenkins build with tag argument\n");
 });
 
 // listen on localhost:8000
